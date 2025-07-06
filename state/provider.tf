2aadcf73ded42131e4e69d0a4686337f853c6f0a
Divyani <divyani.t74@gmail.com>
2025-07-06 20:53:26 +0530

    terraform

---
+++ state/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+
+  backend "s3"{
+    bucket = "div-remote-state"
+    key = "div-remote-state-demo"
+    region = "us-east-1"
+    encrypt = true
+    #dynamodb_table = "div-remote-state"
+    use_lockfile = true #s3 native locking 
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
